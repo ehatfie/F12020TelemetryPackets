38fecebd3989d698b3fe4f10ecd28746037d0b2d
Erik Hatfield <ehhatfield@protonmail.com>
2020-11-11 20:53:06 -0600

    packet header conversion

--- Sources/F12020TelemetryPackets/PacketHeader.swift
+++ Sources/F12020TelemetryPackets/PacketHeader.swift
@@ -9,27 +9,41 @@ import Foundation
 import NIO
 
 struct PacketHeader {
-    let packetFormat: UInt16? // game year packet type eg 2020
-    let gameMajorVersion: UInt8? //uint8 // vX.00
-    let gameMinorVersion: UInt8? //uint8 // v1.XX
-    let packetVersion:  UInt8? //uint8 //version of packet type
-    let packetId: UInt8? //uint8 // packet type
-    let sessionUID: UInt64? //uint64 // unique session identifier
-    let sessionTime: Float? // session timestamp
-    let frameIdentifier: UInt32? // uint32 // Identifier for which frame the packet was retrieved on
-    let playerCarIndex: UInt8? //uint8 // index of players car
-    let secondaryPlayerCarIndex: UInt8? // UInt8? // index of secondary players car, 255 if no second player
+    let packetFormat: Int // game year packet type eg 2020
+    let gameMajorVersion: Int //uint8 // vX.00
+    let gameMinorVersion: Int //uint8 // v1.XX
+    let packetVersion: Int //uint8 //version of packet type
+    let packetId: Int //uint8 // packet type
+    let sessionUID: Int //uint64 // unique session identifier
+    let sessionTime: Float // session timestamp
+    let frameIdentifier: Int // uint32 // Identifier for which frame the packet was retrieved on
+    let playerCarIndex: Int //uint8 // index of players car
+    let secondaryPlayerCarIndex: Int // UInt8? // index of secondary players car, 255 if no second player
     
-    init(data: inout ByteBuffer) throws {
-        self.packetFormat = data.readInteger(endianness: .little, as: UInt16.self)
-        self.gameMajorVersion = data.readInteger(endianness: .little, as: UInt8.self)
-        self.gameMinorVersion = data.readInteger(endianness: .little, as: UInt8.self)
-        self.packetVersion = data.readInteger(endianness: .little, as: UInt8.self)
-        self.packetId = data.readInteger(endianness: .little, as: UInt8.self)
-        self.sessionUID = data.readInteger(endianness: .little, as: UInt64.self)
-        self.sessionTime = data.readFloat()
-        self.frameIdentifier = data.readInteger(endianness: .little, as: UInt32.self)
-        self.playerCarIndex = data.readInteger(endianness: .little, as: UInt8.self)
-        self.secondaryPlayerCarIndex = data.readInteger(endianness: .little, as: UInt8.self)
+    init?(data: inout ByteBuffer) {
+        guard let packetFormat = data.readInt(as: UInt16.self),
+              let gameMajorVersion = data.readInt(as: UInt8.self),
+              let gameMinorVersion = data.readInt(as: UInt8.self),
+              let packetVersion = data.readInt(as: UInt8.self),
+              let packetId = data.readInt(as: UInt8.self),
+              let sessionUID = data.readInt(as: UInt64.self),
+              let sessionTime = data.readFloat(),
+              let frameIdentifier = data.readInt(as: UInt32.self),
+              let playerCarIndex = data.readInt(as: UInt8.self),
+              let secondaryPlayerCarIndex = data.readInt(as: UInt8.self)
+        else {
+            return nil
+        }
+        
+        self.packetFormat = packetFormat
+        self.gameMajorVersion = gameMajorVersion
+        self.gameMinorVersion = gameMinorVersion
+        self.packetVersion = packetVersion
+        self.packetId = packetId
+        self.sessionUID = sessionUID
+        self.sessionTime = sessionTime
+        self.frameIdentifier = frameIdentifier
+        self.playerCarIndex = playerCarIndex
+        self.secondaryPlayerCarIndex = secondaryPlayerCarIndex
     }
 }
