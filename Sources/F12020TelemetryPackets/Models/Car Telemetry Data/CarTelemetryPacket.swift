3e040427c89f360603a25da7f3eb70eae370a9ef
Erik Hatfield <ehhatfield@protonmail.com>
2020-11-15 19:01:25 -0600

    adding CarTelemetryPacket

--- Sources/F12020TelemetryPackets/Models/Car Telemetry Data/CarTelemetryPacket.swift
+++ Sources/F12020TelemetryPackets/Models/Car Telemetry Data/CarTelemetryPacket.swift
@@ -9,17 +9,22 @@ import Foundation
 import NIO
 
 public struct CarTelemetryDataPacket {
-    let header: PacketHeader
-    let carTelemetryData: [CarTelemetryData]
-    let buttonStatus: Int? // uint32 fit flags specifying which buttons are being pressed
-    let mfdPanelIndex: Int? // uint8 Index of MFD Panel open - 255 = MCD closed
-                            // singleplayer, race - 0 =  Car setup, 1 = Pits 2 = Damage, 3 = Enigne, 4 = Temperature
-    let mfdPanelIndexSecondaryPlayer: Int? //UInt8, same as above
-    let suggestedGear: Int? //Int8 //suggested gear for the player (1-8) and 0 if no gear selected
+    public let header: PacketHeader
+    public let carTelemetryData: [CarTelemetryData]
+    public let buttonStatus: Int // uint32 fit flags specifying which buttons are being pressed
+    public let mfdPanelIndex: Int // uint8 Index of MFD Panel open - 255 = MCD closed
+                             // singleplayer, race - 0 =  Car setup, 1 = Pits 2 = Damage, 3 = Enigne, 4 = Temperature
+    public let mfdPanelIndexSecondaryPlayer: Int //UInt8, same as above
+    public let suggestedGear: Int //Int8 //suggested gear for the player (1-8) and 0 if no gear selected
     
     init?(header: PacketHeader, data: inout ByteBuffer) {
         self.header = header
-        guard let packet = CarTelemetryData(data: &data) else { return nil}
+        guard let packet = CarTelemetryData(data: &data),
+              let buttonStatus = data.readInt(as: UInt32.self),
+              let mfdPanelIndex = data.readInt(as: UInt8.self),
+              let mdfPanelIndexSecondaryPlayer = data.readInt(as: UInt8.self),
+              let suggestedGear = data.readInt(as: Int8.self)
+              else { return nil }
         self.carTelemetryData = [packet]
         
         self.buttonStatus = data.readInt(as: UInt32.self)
